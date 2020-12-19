1b39a71674743840491c948e0df39a7ad7fa8680
Lakshay Malhotra <ihybrid11@gmail.com>
2020-12-19 14:32:37 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import GlobalStyle from './theme/globalStyles';
 
 ReactDOM.render(
     <>
-        <GlobalStyle />
         <App />
     </>,
     document.getElementById('root')
